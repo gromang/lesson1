0959ec5c9e8aee0cef560386b5edb52d49d5fe89
Roman M <lp15.mr@yandex.ru>
2020-08-13 17:12:57 +0300

    add

--- types.py
+++ types.py
@@ -1,18 +1,26 @@
-a = 2
-b = 0.5
-print(a + b)
+# a = 2
+# b = 0.5
+# print(a + b)
 
-name = 'РОМАН'
-print(f'Привет, {name.capitalize()}!')
+# name = 'РОМАН'
+# print(f'Привет, {name.capitalize()}!')
 
-name = input('Введите Ваше имя: ')
-print(f'Привет, {name.upper()}! Как дела?')
+# name = input('Введите Ваше имя: ')
+# print(f'Привет, {name.upper()}! Как дела?')
 
 
-v = input('Введите число от 1 до 10: ')
-print(int(v)+10)
+# v = input('Введите число от 1 до 10: ')
+# print(int(v)+10)
 
-print('1.0\n2\nTrue\nFalse\nFalse')
+# print('1.0\n2\nTrue\nFalse\nFalse')
 
-v = "fff"*2
-print(v)
+# v = "fff"*2
+# print(v)
+
+# print(float('1'))
+# print(int(2.5))
+# print(bool(1))
+# print(bool(''))
+# print(bool(0))
+
+print(int(2.8))
